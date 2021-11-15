a67e123d82c714382e8138e022b34858f7a3d316
leesh5000 <leesh50000@gmail.com>
2021-11-15 22:32:01 +0900

    좌표정렬 풀이 완료 및 후위식연산 풀이중

---
+++ Stack&Queue/src/좌표정렬.java
@@ -0,0 +1,41 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.StringTokenizer;
+
+class Point implements Comparable<Point> {
+
+    public int x, y;
+
+    Point(int x, int y) {
+        this.x = x;
+        this.y = y;
+    }
+
+    @Override
+    public int compareTo(Point o) {
+        if (this.x == o.x) {
+            return this.y - o.y;
+        } else {
+            return this.x - o.x;
+        }
+    }
+}
+
+public class 좌표정렬 {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        int n = Integer.parseInt(st.nextToken());
+        ArrayList<Point> arr = new ArrayList<>();
+        for (int i = 0; i < n; i++) {
+            st = new StringTokenizer(br.readLine());
+            int x = Integer.parseInt(st.nextToken());
+            int y = Integer.parseInt(st.nextToken());
+            arr.add(new Point(x, y));
+        }
+        Collections.sort(arr);
+    }
+}
